73e76245d396ce283533bdcf2318690007301ecd
Augustine Smith <absaugustineflash@gmail.com>
2023-10-01 18:41:31 +0000

    feat: add recover middleware

--- internal/api/api.go
+++ internal/api/api.go
@@ -6,6 +6,7 @@ import (
 
 	v1 "github.com/The-Flash/code-racer/internal/api/v1"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/sarulabs/di/v2"
 )
 
@@ -28,6 +29,10 @@ func NewAPI(ctn di.Container) (r *API, err error) {
 	r.app = fiber.New(fiber.Config{
 		ServerHeader: "code-racer",
 	})
+
+	r.app.Use(recover.New())
+
+	// TODO: add rate limtter
 
 	new(v1.Router).Setup(r.app.Group("/api/v1"), ctn)
 	return
