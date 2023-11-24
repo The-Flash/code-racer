1f18fe0243e243e361d9fd2a8be6aec90020e8d2
Augustine Smith <absaugustineflash@gmail.com>
2023-11-24 05:05:38 +0000

    check prlimit exists

--- runners/nodejs/setup.sh
+++ runners/nodejs/setup.sh
@@ -1,4 +1,10 @@
 apk add libseccomp-dev # necessary for nosocket to work
 apk add util-linux # to get prlimit
 
+prlimit
+# check that the previous command worked or exists
+if [ $? -ne 0 ]; then
+    exit
+fi
+
 echo "READY" >> /READY
