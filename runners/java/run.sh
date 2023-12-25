66088af3114dda2acb95e00f3651b224c827217d
Augustine Smith <absaugustineflash@gmail.com>
2023-12-25 12:36:02 +0000

    feat: add java

---
+++ runners/java/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+maxProcesses=$1
+maxOpenFiles=$2
+maxFileSize=$3
+timeoutSecs=$4
+shift 4
+filename=$1
+prlimit --nproc=$maxProcesses --nofile=$maxOpenFiles --fsize=$maxFileSize timeout -s SIGKILL -k 10 10 javac $filename
+classname=$(echo $filename | sed 's/\.java//')
+prlimit --nproc=$maxProcesses --nofile=$maxOpenFiles --fsize=$maxFileSize timeout -s SIGKILL -k 10 10 java $classname $@
