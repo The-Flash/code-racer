28a9614c9121ee8de58218133e17b1eb0cf2a742
Augustine Smith <absaugustineflash@gmail.com>
2023-12-25 13:38:26 +0000

    feat: add ruby

---
+++ runners/ruby/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+maxProcesses=$1
+maxOpenFiles=$2
+maxFileSize=$3
+timeoutSecs=$4
+shift 4
+prlimit --nproc=$maxProcesses --nofile=$maxOpenFiles --fsize=$maxFileSize timeout -s SIGKILL -k $timeoutSecs $timeoutSecs ruby $@ 
---
+++ runners/ruby/setup.sh
@@ -0,0 +1,9 @@
+apk add libseccomp-dev # necessary for nosocket to work
+apk add util-linux # to get prlimit
+
+prlimit
+# check that the previous command worked or exists
+if [ $? -ne 0 ]; then
+    exit
+fi
+echo "READY" >> /READY
